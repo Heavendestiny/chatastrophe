14fe6057fe5bca3b54512124a13be3c733148387
someone <stuff@whatever.com>
2018-04-29 13:04:39 -0400

    css boilerplate snippet

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry:  __dirname + "/src/index.js",
+  output: {
+        path: __dirname + "/public",
+        filename:"bundle.js",
+        publicPath: "/",
+      },
+      devServer: {
+        contentBase: "./public" ,
+        historyApiFallback: true ,
+        inline: true,
+      }
+  };
+
+
+
+
+
+
+
+
+
